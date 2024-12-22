4e78a1c6fdc6e365976b89947b8a52df58c7b6b5
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2024-12-22 11:18:17 +0100

    renaming files

---
+++ thtmlview/pkgIndex.tcl
@@ -0,0 +1,3 @@
+package ifneeded thtmlview::thtmlview 2.0.0 [list source [file join $dir thtmlview.tcl]]
+package ifneeded thtmlview::doctools    0.1 [list source [file join $dir thtmlview-doctools.tcl]]
+package ifneeded thtmlview::mkdoc       0.1 [list source [file join $dir thtmlview-mkdoc.tcl]]
